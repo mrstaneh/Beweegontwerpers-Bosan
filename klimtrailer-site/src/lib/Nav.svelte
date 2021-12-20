2e8dc2174b167b7c1987ff5839828ed49aeb8915
Stan Jaworski <staneh@live.nl>
2021-12-20 13:51:49 +0100

    fixed navbar

---
+++ klimtrailer-site/src/lib/Nav.svelte
@@ -0,0 +1,59 @@
+<script>
+    export let pagePath = undefined;
+</script>
+
+<ul class="nav {pagePath == '/' ? 'homepage' : 'normalpage'}">
+    <a class="navbar-brand" href="index.svelte">
+      <img src="logo.jpg" alt="Logo" width="30px" />
+    </a>
+    <li><a href="">Opdracht</a></li>
+</ul>
+
+<style>
+    .homepage {
+      width: 100%;
+      position: absolute;
+      top: 0;
+      left: 0;
+    }
+
+    .normalpage{
+        margin: 0;
+    }
+  
+    .homepage > * {
+      margin: 0 0;
+    }
+  
+    .nav {
+      list-style-type: none;
+      top: -16px;
+      padding: 0;
+      z-index: 1;
+    }
+  
+    .nav > a > img {
+      padding: 14px;
+    }
+  
+    .nav > li {
+      float: right;
+      font-size: 18pt;
+    }
+  
+    .nav > li > a {
+      display: block;
+      text-align: center;
+      padding: 14px;
+      text-decoration: none;
+    }
+  
+    .nav > li > a:hover {
+      background-color: #004b8e;
+      color: white;
+  
+      -webkit-transition-duration: 200ms; /* Vertraag het hoveren */
+      transition-duration: 200ms;
+    }
+</style>
+  
