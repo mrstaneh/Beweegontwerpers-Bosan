42665e10a943af388fd55f7ffdb6d2db4d213084
Stan Jaworski <staneh@live.nl>
2022-01-11 15:43:12 +0100

    added page transitions

---
+++ klimtrailer-site/src/lib/PageTransitions.svelte
@@ -0,0 +1,10 @@
+<script>
+    import { fade } from 'svelte/transition';
+    export let refresh = '';
+</script>
+  
+{#key refresh}
+    <div in:fade={{ delay: 50, duration: 100 }}>
+        <slot/>
+    </div>
+{/key}
--- klimtrailer-site/src/routes/__layout.svelte
+++ klimtrailer-site/src/routes/__layout.svelte
@@ -1,7 +1,10 @@
 <script>
     import Nav from "$lib/Nav.svelte";
     import { page } from '$app/stores';
+    import PageTransitions from "$lib/PageTransitions.svelte";
 </script>
 
 <Nav pagePath={$page.path}/>
-<slot />
+<PageTransitions refresh={$page.path}>
+    <slot />
+</PageTransitions>
