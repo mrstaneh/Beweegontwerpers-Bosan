87048aa8becb74a1369a751b567e1f9b0f90b57b
Rick Peters <r.peters-ab@student.fontys.nl>
2022-01-12 14:22:08 +0100

    Corrected merge

--- klimtrailer-site/src/routes/sprint7.svelte
+++ klimtrailer-site/src/routes/sprint7.svelte
@@ -1,22 +1,20 @@
 <script>
-    import TimelineSprint1 from "$lib/TimelineSprint1.svelte";
-  </script>
-  
-  <svelte:head>
-    <title>Sprint 6</title>
-  </svelte:head>
-  
-  <main>
-    <div class="left-bar" />
-    <div class="content">
-        <div class="section">
-            <h1>Sprint 7: Afronding</h1>
-        </div>
-        <div class="section mixandmatch" id="mixandmatch">
-            <h1>Afronding</h1>
-        </div>
+  import TimelineSprint1 from "$lib/TimelineSprint1.svelte";
+</script>
+
+<svelte:head>
+  <title>Sprint 6</title>
+</svelte:head>
+
+<main>
+  <div class="left-bar" />
+  <div class="content">
+    <h1>Sprint 7: Afronding</h1>
+    <div class="section mixandmatch" id="mixandmatch">
+      <h1>Afronding</h1>
     </div>
-    <div class="right-bar">
-      <TimelineSprint1 />
-    </div>
-  </main>
+  </div>
+  <div class="right-bar">
+    <TimelineSprint1 />
+  </div>
+</main>
