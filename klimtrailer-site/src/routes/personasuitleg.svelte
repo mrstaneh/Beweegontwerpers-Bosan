596f38f095be7c78d175734edc97cd3cae4584a4
Stan Jaworski <staneh@live.nl>
2022-01-14 11:39:42 +0100

    moved personas to seperate page

---
+++ klimtrailer-site/src/routes/personasuitleg.svelte
@@ -0,0 +1,52 @@
+<script>
+    import TimelineSprint1 from "$lib/TimelineSprint1.svelte";
+  </script>
+  
+  <svelte:head>
+    <title>Uitleg Persona's</title>
+  </svelte:head>
+  
+  <main>
+    <div class="left-bar" />
+    <div class="content">
+      <h1>Uitleg Persona's</h1>
+      <div class="section personas" id="personas">
+        <h1>Personas</h1>
+        <p style="color: #4491DA;">Hier zullen onze persona’s toegelicht worden. Dit zal gaan over de leraren en leerlingen. Je kunt de personas op <a href="/personas" target="_blank">deze</a> pagina vinden.</p>
+  
+        <p style="color: #F06119; display: inline;">De basis in het kort</p>
+        <p>In het begin van het project zijn er meerdere persona’s ontstaan. Deze persona’s geven een duidelijke weergave van onze doelgroep.</p>
+  
+        <p style="color: #F06119; display: inline;">Voordelen</p>
+        <p>Een voordeel van de persona’s is dat je vanuit die persoon kunt ontwerpen. Je gaat dus echt een persona bril opzetten. Tevens staat een persona niet vast. Een persona zal voortdurend evolueren.</p>
+  
+        <p style="color: #F06119; display: inline;">Validatie</p>
+        <p>Een ander belangrijk punt is dat er een validatie moet zijn. Dit is gedaan door onder andere de persona’s te presenteren aan Anoek. Zij heeft gekeken of de persona’s pasten bij een stereotype basisschoolkind. Hierbij gaf Anoek aan dat er tevens veel verschillende basisschoolkinderen zijn         . Het is ook mogelijk dat je een persona afstoot. Sommige persona’s zijn van meerwaarde op het ontwerpproces. Hierbij is iedere persona afgegaan en is er gekeken wat er belangrijk is bij het product. 
+          Daarnaast is er meermaals feedback gevraagd op onze persona’s door Mark Janssen. Dankzij deze feedback konden onze persona’s telkens relevanter gemaakt worden. Uiteraard hebben diverse interviews en schoolbezoeken ons ook erg geholpen bij het creëeren van geloofwaardige persona’s.
+          </p>
+  
+          <p style="color: #F06119; display: inline;">Onderbouwing keuzes in proces naar aanleiding van de persona's</p>
+          <p>Tijdens het proces zijn er belangrijke keuzes gemaakt aan de hand van de persona’s. een aantal van deze keuzes zijn hieronder uitgelicht en onderbouwd.</p>
+          <p>De persona’s die het meest bepalend zijn geweest voor het proces zijn Rafaël, Nicky, Luca en Amy. Hun karaktereigenschappen hebben voor het overgrote deel gezorgd voor toevoegingen aan de Boulder Mini. Al deze toevoegingen zijn er om de Boulder Mini voor iedereen zo goed als mogelijk te laten voldoen aan hun wensen. </p>
+          <p>Aan het begin van het ontwerpproces zijn er verschillende interviews gehouden met verschillende experts. Tijdens deze interviews kwam al snel naar voren dat docenten toestellen vaak ook op andere manieren gebruiken dan alleen daar waar ze voor bedoeld zijn. Joris is één van deze docenten en gebruikt tijdens zijn les de toestellen vaak op een creatieve manier. Joris heeft er dus voor gezorgd dat de Boulder Mini niet alleen te gebruiken is als klimmuur maar ook toepasbaar moet zijn op andere manieren. </p>
+          <p>Er zitten veel positieve kanten aan een creatieve instelling, deze moeten dus ook gestimuleerd worden. Naar aanleiding hiervan is er dus nagedacht over mogelijkheden om dit te realiseren, hier kwam het idee uit om verschillende wanden te bedenken. Dit zorgt ervoor dat de Boulder Mini veelzijdiger is en stimuleert daarmee ook de creatieve kant van de docenten.</p>
+          <p>Aansluitend hierop is het ook de bedoeling dat de kinderen voldoende nieuwe dingen leren tijdens de les. Voor de docenten is het dus ook van belang dat er voldoende en verschillende leerlijnen worden geraakt met de Boulder Mini, dit is met name voor Marc en Nicky heel belangrijk.</p>
+          <p>Verder vinden Nicky en de andere docenten het heel belangrijk dat er verschillende niveaus mogelijk moeten zijn bij het gebruik van de Boulder Mini. Dit omdat er dan op veel verschillende niveaus les gegeven kan worden. Enerzijds om ervoor te zorgen dat alle kinderen mee kunnen doen en anderzijds om te zorgen dat alle kinderen constant opnieuw uitgedaagd worden. 
+            Maar niet alleen voor de docenten was dit een eis, ook voor de kinderen is het belangrijk dat er verschillende niveaus mogelijk zijn. Hierdoor zullen ze zich niet buitengesloten voelen omdat ze allemaal mee kunnen doen en zullen ze gemotiveerd blijven omdat ze steeds opnieuw uitgedaagd worden.
+            </p>
+            <p>Een nieuw toestel zorgt logischerwijs voor een hoop vragen. Het is dan ook heel belangrijk dat de docenten het nieuwe toestel goed leren kennen en gebruiken. Hierbij is een handleiding noodzakelijk en ook een logische stap in het proces. Voor Amy in het bijzonder, zij heeft namelijk minder ervaring met toestellen en voor haar is het dan ook een eis dat de Boulder Mini simpel en veilig te gebruiken is. </p>
+            <p>Om goed af te sluiten is er ook al nagedacht over de toekomst. Een mooie eventuele toevoeging zou zijn om verlichting in de grepen te verwerken. Met deze optie krijg je nog meer mogelijkheden om de verschillende routes uit te zetten en op deze manier wordt het dus nog makkelijker om de route aan te passen aan het kind. Dit zou met name voor Luca en Mariec een toevoeging zijn. </p>
+            <p style="font-style: italic;">Om goed af te sluiten is er ook al nagedacht over de toekomst. Een mooie eventuele toevoeging zou zijn om verlichting in de grepen te verwerken. Met deze optie krijg je nog meer mogelijkheden om de verschillende routes uit te zetten en op deze manier wordt het dus nog makkelijker om de route aan te passen aan het kind. Dit zou met name voor Luca en Mariec een toevoeging zijn. </p>
+            <p style="font-style: italic;">Verder zou met deze verlichting ook gedacht kunnen worden aan het gevoel van succes. Voornamelijk Luca en Rafaël zouden hier profijt van hebben. Zij zijn gemotiveerder als ze worden aangemoedigd en deze eventuele verlichting zou hierbij helpen.</p>
+      </div>
+    </div>
+    <div class="right-bar">
+      <TimelineSprint1 />
+    </div>
+  </main>
+  
+  <style>
+    p {
+      margin-top: 0;
+    }
+  </style>
